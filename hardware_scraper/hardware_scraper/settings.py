4128cafe40ee52392510538347c476d7582ff810
Johny Chen Jy <johnychenjy@gmail.com>
2021-08-26 01:40:03 -0300

    adding splash and changing scrapying targets

--- hardware_scraper/hardware_scraper/settings.py
+++ hardware_scraper/hardware_scraper/settings.py
@@ -12,11 +12,14 @@ BOT_NAME = 'hardware_scraper'
 SPIDER_MODULES = ['hardware_scraper.spiders']
 NEWSPIDER_MODULE = 'hardware_scraper.spiders'
 
+# Splash Setup
+SPLASH_URL = 'http://localhost:8050'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'hardware_scraper (+http://www.yourdomain.com)'
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 " \
-             "(KHTML, like Gecko) Chrome/32.0.1700.102 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+             "Chrome/58.0.3029.110 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,10 +30,10 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 15
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_IP = 3
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
@@ -47,15 +50,15 @@ COOKIES_ENABLED = False
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
-    'hardware_scraper.middlewares.HardwareScraperSpiderMiddleware': 543,
-    'hardware_scraper.downloadermiddlewares.redirect.MetaRefreshMiddleware': None,
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
 }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    'hardware_scraper.middlewares.HardwareScraperDownloaderMiddleware': 543,
-    'hardware_scraper.downloadermiddlewares.redirect.MetaRefreshMiddleware': None,
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
 }
 METAREFRESH_ENABLED = False
 # Enable or disable extensions
@@ -74,12 +77,12 @@ METAREFRESH_ENABLED = False
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
@@ -89,6 +92,6 @@ HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
-REDIRECT_ENABLED = False
+FEED_EXPORT_ENCODING = "utf-8"
--- hardware_scraper/hardware_scraper/spiders/video_cards.py
+++ hardware_scraper/hardware_scraper/spiders/video_cards.py
@@ -6,62 +6,71 @@ class VideoCardsSpider(scrapy.Spider):
     name = 'video_cards'
     allowed_domains = [
         'pichau.com.br/',
-        'webcache.googleusercontent.com',
-        'kabum.com.br',
+        'localhost:8050/',
     ]
     start_urls = [
-        'https://www.pichau.com.br/hardware/placa-de-video',
-        'https://webcache.googleusercontent.com/search?q=cache:https:'
-        '//www.terabyteshop.com.br/hardware/placas-de-video',
-        'https://webcache.googleusercontent.com/search?q=cache:'
-        'https://kabum.com.br/hardware/placa-de-video-vga',
+        'http://localhost:8050/render.html?url=https://www.pichau.com.br/hardware/placa-de-video',
+        'http://localhost:8050/render.html?url=https://www.amazon.com.br/b?node=16364811011&ref=lp_16364750011_nr_n_9',
+        'http://localhost:8050/render.html?url=https://www.chipart.com.br/placa-de-video',
     ]
 
     def parse(self, response):
-        if response.request.url == 'https://www.pichau.com.br/hardware/placa-de-video':
-            self._parse_pichau(response)
-        elif response.request.url == 'https://webcache.googleusercontent.com/search?q=cache:' \
-                                     'https://www.terabyteshop.com.br/hardware/placas-de-video':
-            self._parse_terabyte(response)
-        elif response.request.url == 'https://kabum.com.br/hardware/placa-de-video-vga':
-            self._parse_kabum(response)
+        if response.url.split('=', 1)[1].split('/', 3)[2] == 'www.pichau.com.br':
+            page = 1
+            products = response.css('div.MuiCardContent-root')
 
-    def _parse_pichau(self, response):
-        products = response.css('div.MuiCardContent-root')
-        for product in products:
-            product_data = {
-                'nome': product.css('h2::text').get(),
-                'preco_a_vista': product.css('div::text')[1].get(),
-                'preco_parcelado': product.css('div::text')[2].get(),
-                'url': response.url,
-            }
-            yield product_data
-        next_page = response.css('').attrib['href']
+            for product in products:
+                if not product.css('div::text'):
+                    page = 0
+                    break
+                product_data = {
+                    'nome': product.css('h2::text').get(),
+                    'preco_a_vista': 'R$' + product.css('div::text')[1].get(),
+                    'preco_parcelado': re.sub('[^R$0-9,]', '', product.css('div::text')[2].get()),
+                    'url': response.url.split('=', 1)[1],
+                }
+                yield product_data
+            if page:
+                page += 1
+                yield scrapy.Request('http://localhost:8050/render.html?url=https://www.pichau.com.br/hardware/placa-de'
+                                     '-video?page=%s' % page, callback=self.parse)
 
-    def _parse_kabum(self, response):
-        f = open('kabum_html.html', 'a')
-        f.write(response.txt)
-        f.close()
+        elif response.url.split('=', 1)[1].split('/', 3)[2] == 'www.amazon.com.br':
+            products = response.css('div.sg-col-4-of-12')
+            for product in products:
+                product_data = {
+                    'nome': product.css('h2').css('span::text').get(),
+                    'preco_a_vista': re.sub('[^R$0-9,]', '', product.css('span.a-offscreen::text').get()),
+                    'preco_parcelado': re.sub('[^R$0-9,]', '', product.css('span.a-offscreen::text').get()),
+                    'url': response.url.split('=', 1)[1],
+                }
+                if not product_data['preco_a_vista']:
+                    continue
 
-        # products = response.css('div.productCard')
-        # for product in products:
-        #     product_data = {
-        #         'nome': product.css('a::text').get(),
-        #         'preco_a_vista': product.css('div::text').get(),
-        #         'preco_parcelado': product.css('span::text').get(),
-        #         'url': response.url,
-        #     }
-        #     yield product_data
+                yield product_data
+            next_page = response.xpath(
+                '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[32]/span/div/div/ul/li[7]/a/@ref')
 
-    def _parse_terabyte(self, response):
-        products = response.css('div.commerce_columns_item_inner')
+            if next_page:
+                yield scrapy.Request('http://localhost:8050/render.html?url=https://www.amazon.com.br' + next_page,
+                                     callback=self.parse)
 
-        for product in products:
-            product_data = {
-                'nome': product.css('a').attrib['title'],
-                'preco_a_vista': product.css('div.prod-new-price').css('span::text').get(),
-                'preco_parcelado': str(12 * float(re.sub('[^0-9,]', '', product.xpath(
-                    '//*[@id="prodarea"]/div[1]/div/div[4]/div[1]/div[3]/span[2]').get()))),
-                'url': response.url,
-            }
-            yield product_data
+        elif response.url.split('=', 1)[1].split('/', 3)[2] == 'www.chipart.com.br':
+            products = response.css('div.product-card')
+
+            for product in products:
+                product_data = {
+                    'nome': product.css('div.product-title').css('h2::text').get(),
+                    'preco_a_vista': product.css('div.billet').css('span.price.total::text').get(),
+                    'preco_parcelado': product.css('div.creditcard').css('span.price.total::text').get(),
+                    'url': response.url.split('=', 1)[1],
+                }
+                if not product_data['preco_a_vista']:
+                    return
+                yield product_data
+
+            next_page = response.xpath('/html/body/div[1]/main/div[1]/div[2]/div/div[2]/div[2]/section/div/div['
+                                       '3]/div/div/div[3]/div/div[3]/a/@href')
+            if next_page:
+                yield scrapy.Request('http://localhost:8050/render.html?url=https://www.chipart.com.br' + next_page,
+                                     callback=self.parse)
