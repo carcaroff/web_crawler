baf0e5afb6fca9fd5cfd86cba1e48968bb65f0e5
Venâncio <joaovitorvenancio99@outlook.com>
2021-09-01 23:28:39 -0300

    new window
    added a new window

--- gui/gui.py
+++ gui/gui.py
@@ -17,14 +17,141 @@ window.geometry("1440x1024")
 window.configure(bg = "#FFFFFF")
 
 def mainMenu():
-    newWindow = Toplevel(window)
-    
-    newWindow.title("New Window")
- 
-    newWindow.geometry("1440x1024")
- 
-    Label(newWindow,
-          text ="This is a new window").pack()
+
+    window.configure(bg = "#FFFFFF")
+
+    canvas = Canvas(
+        window,
+        bg = "#FFFFFF",
+        height = 1024,
+        width = 1440,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge"
+    )
+
+    canvas.place(x = 0, y = 0)
+
+    canvas.create_rectangle(
+        0.0,
+        0.0,
+        1440.0,
+        1024.0,
+        fill="#FFFFFF",
+        outline="")
+
+    image_image_1 = PhotoImage(
+        file=relative_to_assets("select_menu/image_1.png"))
+
+    image_1 = canvas.create_image(
+        719.0,
+        511.0,
+        image=image_image_1
+    )
+
+    image_image_2 = PhotoImage(
+        file=relative_to_assets("select_menu/image_2.png"))
+
+    image_2 = canvas.create_image(
+        366.0,
+        511.0,
+        image=image_image_2
+    )
+
+    button_image_1 = PhotoImage(
+        file=relative_to_assets("select_menu/button_1.png"))
+
+    button_1 = Button(
+        image=button_image_1,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: print("select_menu/button_1 clicked"),
+        relief="flat"
+    )
+
+    button_1.place(
+        x=123.0,
+        y=417.0,
+        width=481.0,
+        height=111.0
+    )
+
+    button_image_2 = PhotoImage(
+        file=relative_to_assets("select_menu/button_2.png"))
+
+    button_2 = Button(
+        image=button_image_2,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: print("select_menu/button_2 clicked"),
+        relief="flat"
+    )
+
+    button_2.place(
+        x=835.0,
+        y=417.0,
+        width=481.0,
+        height=111.0
+    )
+
+    button_image_3 = PhotoImage(
+        file=relative_to_assets("select_menu/button_3.png"))
+
+    button_3 = Button(
+        image=button_image_3,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: print("select_menu/button_3 clicked"),
+        relief="flat"
+    )
+
+    button_3.place(
+        x=835.0,
+        y=698.0,
+        width=481.0,
+        height=111.0
+    )
+
+    button_image_4 = PhotoImage(
+        file=relative_to_assets("select_menu/button_4.png"))
+
+    button_4 = Button(
+        image=button_image_4,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: print("select_menu/button_4 clicked"),
+        relief="flat"
+    )
+
+    button_4.place(
+        x=123.0,
+        y=698.0,
+        width=481.0,
+        height=111.0
+    )
+
+    image_image_3 = PhotoImage(
+        file=relative_to_assets("select_menu/image_3.png"))
+
+    image_3 = canvas.create_image(
+        719.0,
+        125.0,
+        image=image_image_3
+    )
+
+    canvas.create_text(
+        360.0,
+        100.0,
+        anchor="nw",
+        text="ESCOLHA UMA OPÇÃO:",
+        fill="#FFFFFF",
+        font=("Roboto Bold", 68 * -1),
+        width=1039.0
+    )
+
+    window.resizable(False, False)
+
+    window.mainloop()
 
 canvas = Canvas(
     window,
@@ -127,3 +254,4 @@ canvas.create_rectangle(
 window.resizable(False, False)
 
 window.mainloop()
+
